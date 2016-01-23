7b50217d840e6aef994b4cb6139710a10a4e8cae
Ben Diuguid <Bendiuguid@gmail.com>
2016-01-23 16:36:18 -0500

    hopefully fixed db model

--- front-end/pages/Create.jsx
+++ front-end/pages/Create.jsx
@@ -1,9 +1,26 @@
+const {
+  Row,
+  Col
+} = bootstrap;
+
 Create = React.createClass({
 
   render() {
     return (
       <div>
-        <h1>Create</h1>
+        <Row>
+          <Col xs={12} md={12} lg={12}>
+            <h1>Create</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={8} md={8} lg={8}>
+            
+          </Col>
+          <Col xs={4} md={4} lg={4}>
+
+          </Col>
+        </Row>
       </div>
     );
   }
